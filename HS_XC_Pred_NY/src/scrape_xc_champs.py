aa8f0d8d5e7dd4760297019ccbd1380396d65519
andres-orbegozo <112585303+andres-orbegozo@users.noreply.github.com>
2024-05-21 01:58:44 +0000

    should work for MORE results

--- HS_XC_Pred_NY/src/scrape_xc_champs.py
+++ HS_XC_Pred_NY/src/scrape_xc_champs.py
@@ -5,74 +5,76 @@ import pandas as pd
 meet_id_list = ['563515', '578154', '563514', '579610', '563689', '563523', '563513', '566096', '580363', '566109', '567388', '564844', '566097', '567648', '567649', '563096']
 
 def race_results_from_link(link):
-    site = requests.get(f'{link}')
+    site = requests.get(link)
     soupy = BeautifulSoup(site.content, 'html.parser')
     results = soupy.find('div', id='meetResultsBody')
     results_text = results.find('pre').text
-    justRunners = results_text.replace('BOYS CLASS A 5K', '').replace('-', '').replace('PLACE', '').replace('PTS', '').replace('GRADE', '').replace('SCHOOL', '').replace('TIME','').replace('PACE','').replace('NAME','').replace('  ','##').replace(' ','').replace('##',' ')
-    splittedRunners = justRunners.split('\r\n')
 
-    splRun_list = []
-    for runner in splittedRunners:
-        splRun_list.append(runner)
+    noise_gone = results_text.replace('-','').replace('=','').replace('  ','##').replace(' ','').replace('##',' ')
+    splitted = noise_gone.split('\r\n')
 
-    splRun_listList = []
-    for runner in splRun_list:
-        temp = runner.split(' ')
-        splRun_listList.append(temp)
+    spl_listList = []
+    for obj in splitted:
+        temp = obj.split(' ')
+        spl_listList.append(temp)
 
     # removes empty and space strings to make all sublists to have the same amount of elements
-    for i in range(len(splRun_listList)):
+    for i in range(len(spl_listList)):
         j=0
-        while j < len(splRun_listList[i]):
+        while j < len(spl_listList[i]):
             # print(i, j)
-            element = splRun_listList[i][j]
+            element = spl_listList[i][j]
             if element == '':
-                splRun_listList[i].remove(element)
+                spl_listList[i].remove(element)
             elif element == ' ':
-                splRun_listList[i].remove(element)
+                spl_listList[i].remove(element)
             else:
                 j=j+1
 
-    # remove empty lists & title
-    splRun_listList = splRun_listList[1:]
-    splRun_listList = [notempty for notempty in splRun_listList if notempty]
+    # rid of empty lists
+    spl_listList = [notempty for notempty in spl_listList if notempty]
 
-    #remove random 4th col number
-    for i in range(len(splRun_listList)):
-        if splRun_listList[i][3] in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
-            del splRun_listList[i][3] 
-    places = []
-    names = []
-    grades = []
-    teams = []
-    times = []
+    #rid of lists less than 4 length and over 6 length
+    spl_listList = [correct for correct in spl_listList if len(correct) >= 4]
 
-    for list in splRun_listList:
-        places.append(list[0])
-        names.append(list[1])
-        grades.append(list[2])
-        teams.append(list[3])
-        times.append(list[4])
+    # create list of ints as strings for removing 'points' values
+    int_list = [i for i in range(1, 501)]
+    str_int_list = [str(num) for num in int_list]
 
-    #final cleaning
-    i=0
-    for name in names:
-        for char in name:
-            if char.isdigit() == True:
-                rep = names[i].replace(char, '')
-                names[i] = rep
-        i=i+1
-    j=0
-    for grade in grades:
-        for char in grade:
-            if char.isdigit() == True:
-                rep = grades[j].replace(char, '')
-                grades[j] = rep
-        j=j+1
+    # remove random 4th col number
+    for i in range(len(spl_listList)):
+        if spl_listList[i][3] in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            del spl_listList[i][3]
+        elif spl_listList[i][2] in str_int_list:
+            del spl_listList[i][2]
+    spl_listList = [correct for correct in spl_listList if len(correct) <= 6]
 
-    #turn to df
-    results_df = pd.DataFrame({'PLACE': places, 'NAME': names, 'GRADE': grades, 'TEAM': teams, 'TIME': times})  
+    #heading extraction
+    heading = spl_listList.pop(0)
+    heading = [scoreless for scoreless in heading if scoreless != 'Score']
+
+    # drop data that is too difficult
+    spl_listList = [nondifficult for nondifficult in spl_listList if len(nondifficult) == len(heading)]
+
+    spl_listList
+
+    # NEW FUNCTION TO CREATE A DF BASED ON VARIABLE COLUMNS             
+
+    numCols = len(heading)
+    listOfLists = []
+    for i in range(numCols):
+        tempList = []
+        for line in spl_listList:
+            tempList.append(line[i])
+        listOfLists.append(tempList)
+
+    ziptied = zip(heading, listOfLists)
+
+    df_dict = {key: value for key, value in ziptied}
+
+    results_df = pd.DataFrame(df_dict)
+
+    results_df  
 
     return results_df
 
@@ -103,7 +105,7 @@ def extract_meet_results(meet_id):
 
     return meet_df
 
-df = extract_meet_results('563514')
+df = extract_meet_results('563515')
 print(df)
 
         
